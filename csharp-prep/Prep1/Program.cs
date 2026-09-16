0d1ead066e032f75bc546b6743585a5b83020155
Joshua-Baird <bai25015@byui.edu>
2026-09-16 09:54:14 -0600

    make a program that intorduces you in the famos james bond style

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string firstName;
+        firstName = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string lastName;
+        lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
