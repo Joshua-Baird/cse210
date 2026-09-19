20dd3818e91711800d2ea7b5d4a548c2ac9d7bb0
Joshua-Baird <bai25015@byui.edu>
2026-09-19 12:15:33 -0600

    complete prep 3 activity

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int userNumber;
+        Console.WriteLine("what is the magic number");
+        do
+        {
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (userNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+        } while (userNumber != magicNumber);
     }
 }
