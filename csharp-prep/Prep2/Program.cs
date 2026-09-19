60b99fa69d7f15a4181a4f64b8da0f30542ede54
Joshua-Baird <bai25015@byui.edu>
2026-09-19 11:46:56 -0600

    complete the prep 2 activity

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your percentage");
+        string percentString = Console.ReadLine();
+        int percent = int.Parse(percentString);
+        string letterGrade;
+        if (percent >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (percent >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (percent >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (percent >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        Console.WriteLine($"Your grade is {letterGrade}");
+        if (letterGrade != "D" && letterGrade != "F")
+        {
+            Console.WriteLine("Your passing! Good job");
+        }
+        else
+        {
+            Console.WriteLine("Try a bit harder, you got this!");
+        }
+
     }
 }
