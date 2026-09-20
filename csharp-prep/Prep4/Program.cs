1fc2c882ec4851217f087c8305f12a8bd62cf53c
Joshua-Baird <bai25015@byui.edu>
2026-09-19 21:49:11 -0600

    complete the prep 4 assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -2,8 +2,30 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    List<int> numbers = new List<int>();
+    Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+    int userNumber;
+    do
     {
-        Console.WriteLine("Hello Prep4 World!");
+      Console.WriteLine("Enter a number: ");
+      userNumber = int.Parse(Console.ReadLine());
+      numbers.Add(userNumber);
+    } while (userNumber != 0);
+    int sum = 0;
+    double max = double.NegativeInfinity;
+    foreach (int number in numbers)
+    {
+      sum = sum + number;
+      if (max < number)
+      {
+        max = number;
+      }
     }
+    int average = sum / numbers.Count;
+    Console.WriteLine($"Total: {sum}");
+    Console.WriteLine($"Average: {average}");
+    Console.WriteLine($"Max: {max}");
+  }
 }
