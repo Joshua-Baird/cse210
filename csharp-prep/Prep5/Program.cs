75d48cd356cc7271d1267a3698f0686335f0be1f
Joshua-Baird <bai25015@byui.edu>
2026-09-19 22:17:30 -0600

    complete the prep 5 assigment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromtUserName();
+        int number = PromtUserNumber();
+        int birthYear = 0;
+        PromtUserBirthYear(out birthYear);
+        int numberSquared = SquareNumber(number);
+        DisplayResult(name, numberSquared, birthYear);
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromtUserName()
+    {
+        Console.WriteLine("Please type your name");
+        return Console.ReadLine();
+    }
+    static int PromtUserNumber()
+    {
+        Console.WriteLine("Please type your favorate number");
+        return int.Parse(Console.ReadLine());
+    }
+    static void PromtUserBirthYear(out int birthYear)
+    {
+        Console.WriteLine("Please type your birth year");
+        birthYear = int.Parse(Console.ReadLine());
+    }
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+    static void DisplayResult(string name, int number, int birthyear)
+    {
+        Console.WriteLine($"{name}, your number squared is {number}");
+        int currentYear = DateTime.Now.Year;
+        int age = currentYear - birthyear;
+        Console.WriteLine($"{name}, you are turning {age} this year");
     }
 }
